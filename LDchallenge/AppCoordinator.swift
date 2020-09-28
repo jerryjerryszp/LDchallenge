ab81500e06a1b87a57c63559de6374f9837088d4
Zipei Jerry Shi <jerryjerryszp@gmail.com>
2020-09-28 14:00:13 -0400

    Setup coordinator

---
+++ LDchallenge/AppCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  AppCoordinator.swift
+//  LDchallenge
+//
+//  Created by Jerry Shi on 2020-09-28.
+//  Copyright © 2020 jerryszp6116. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator {
+    
+    private var window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    
+    func start() {
+        let viewController = ViewController.instantiate()
+        
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
--- LDchallenge/ViewController.swift
+++ LDchallenge/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    static func instantiate() -> ViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let viewController = storyboard.instantiateInitialViewController() as! ViewController
+        return viewController
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
